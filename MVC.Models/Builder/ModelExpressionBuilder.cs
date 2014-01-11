e436d9e183632609ba37262dc93b81cf72c64ab4
ivaylokenov <ivaylo.kenov@gmail.com>
2014-01-12 00:24:02 +0200

    Implemented all fluent methods for ModelExpressionBuilder.cs (#12)

--- MVC.Models/Builder/ModelExpressionBuilder.cs
+++ MVC.Models/Builder/ModelExpressionBuilder.cs
@@ -13,8 +13,12 @@
         private const string NamesNullExceptionMessage = "Binding names cannot be null";
         private const string TypesParameter = "types";
         private const string TypesNullExceptionMessage = "Binding types cannot be null";
+        private const string CategoryNameParameter = "name";
+        private const string CategoryNameNullOrEmptyMessage = "Category name cannot be null or empty";
+        private const string CategoriesParameter = "categories";
+        private const string CategoriesNullMessage = "Categories cannot be null";
 
-        private BindingOptions options;
+        private readonly BindingOptions options;
 
         public ModelExpressionBuilder() 
         {
@@ -55,43 +59,66 @@
 
         public IGenericModelExpressionBuilder<TBase> ExcludeNames(params string[] names)
         {
-            throw new NotImplementedException();
+            if (names == null) throw new ArgumentNullException(NamesParameter, NamesNullExceptionMessage);
+            
+            options.ExcludedNames.AddRange(names);
+            return this;
         }
 
         public IGenericModelExpressionBuilder<TBase> ExcludeNames(IList<string> names)
         {
-            throw new NotImplementedException();
+            if (names == null) throw new ArgumentNullException(NamesParameter, NamesNullExceptionMessage);
+
+            options.ExcludedNames.AddRange(names);
+            return this;
         }
 
         public IGenericModelExpressionBuilder<TBase> ExcludeTypes(params BindingType[] types)
         {
-            throw new NotImplementedException();
+            if (types == null) throw new ArgumentNullException(TypesParameter, TypesNullExceptionMessage);
+
+            options.ExcludedTypes.AddRange(types);
+            return this;
         }
 
         public IGenericModelExpressionBuilder<TBase> ExcludeTypes(IList<BindingType> types)
         {
-            throw new NotImplementedException();
+            if (types == null) throw new ArgumentNullException(TypesParameter, TypesNullExceptionMessage);
+
+            options.ExcludedTypes.AddRange(types);
+            return this;
         }
 
         public IGenericModelExpressionBuilder<TBase> Cachable(bool cachable)
         {
-            throw new NotImplementedException();
+            options.Cachable = cachable;
+            return this;
         }
 
         public IGenericModelExpressionBuilder<TBase> Category(string name)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(name)) throw new ArgumentNullException(CategoryNameParameter, CategoryNameNullOrEmptyMessage);
+
+            options.Categories.Add(name);
+            return this;
         }
 
         public IGenericModelExpressionBuilder<TBase> Categories(params string[] categories)
         {
-            throw new NotImplementedException();
+            if (categories == null) throw new ArgumentNullException(CategoriesParameter, CategoriesNullMessage);
+
+            options.Categories.AddRange(categories);
+            return this;
         }
 
         public IGenericModelExpressionBuilder<TBase> Categories(IList<string> categories)
         {
-            throw new NotImplementedException();
+            if (categories == null) throw new ArgumentNullException(CategoriesParameter, CategoriesNullMessage);
+
+            options.Categories.AddRange(categories);
+            return this;
         }
+
         public Expression<Func<TBase, TResult>> To<TResult>()
         {
             throw new NotImplementedException();
