bb58e46b2c95754dbc6a243b2f5b4e2025bd9c0b
ivaylokenov <ivaylo.kenov@gmail.com>
2014-01-11 16:43:46 +0200

    IModelExpression.cs implemented (#2)

--- MVC.Models/Design/IModelExpression.cs
+++ MVC.Models/Design/IModelExpression.cs
@@ -1,14 +1,25 @@
 ﻿namespace MVC.Models.Design
 {
+    using System.Collections;
+    using System.Collections.Generic;
+
     public interface IModelExpression
     {
         IModelExpression Include(params string[] names);
 
+        IModelExpression Include(IList<string> names);
+        
         IModelExpression Include(params BindType[] types);
+
+        IModelExpression Include(IList<BindType> types);
 
         IModelExpression Exclude(params string[] names);
 
+        IModelExpression Exclude(IList<string> names);
+
         IModelExpression Exclude(params BindType[] types);
+
+        IModelExpression Exclude(IList<BindType> types);
         
         IModelExpression Cachable(bool cachable);
 
@@ -17,5 +28,7 @@
         IModelExpression Category(string name);
 
         IModelExpression Categories(params string[] categories);
+
+        IModelExpression Categories(IList<string> categories);
     }
 }
